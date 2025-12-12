089e4dca470bc8075d71da5ac0d605e9dd8d1f8a
Carlos <carlosrei2505@gmail.com>
2025-12-12 16:07:19 +0100

    Añadidos header y comentario inicial

---
+++ scripts/app_abandono.py
@@ -0,0 +1,4 @@
+Script para analizar abandono de clientes >> scripts/app_abandono.py
+echo Script para analizar abandono de clientes >> scripts/app_abandono.py
+echo Script para analizar abandono de clientes
+#Script para analizar abandono de clientes
